d0ec1630bb4aa3be9b06849f96eab3473190b34d
Julián Melero <hola@julianmelero.com>
2021-09-28 14:14:57 +0200

    Start Borracho

---
+++ aleatorio/borracho.py
@@ -0,0 +1,10 @@
+class Borracho:
+    def __init__(self, name):
+        self.name = name
+
+
+class TraditionalBorracho(Borracho):
+    def __init__(self, name):
+            super.__init__(name)
+
+    def 
